b35634277220855f38f121fb7e4a3a944ad88a1f
Artemisia-gt <prgiraud@gmail.com>
2014-01-13 21:40:05 +0100

    snipe update

--- snipe/snipe.js
+++ snipe/snipe.js
@@ -427,7 +427,7 @@ var txt = '<strong> Village ciblé </strong>
   document.getElementById("paged_view_content").innerHTML = s;
   
   
-  }else{
+  else{
 var link = link.substring(0,link.indexOf("&")) + "&mode=combined&group=0&screen=overview_villages";
 window.location = link;
 
