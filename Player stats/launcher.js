f77bf5ee85c74b4dd17226f54a85e03c9378c8d6
Artemisia-gt <prgiraud@gmail.com>
2014-02-11 00:43:07 +0100

    update stats

--- Player stats/launcher.js
+++ Player stats/launcher.js
@@ -1,1 +1,1 @@
-script="https://raw.github.com/Artemisia-gt/Tribal-Wars-Scripts/master/Player%20stats/player-stats.js";s=document.createElement("script");s.type="text/javascript";s.src=script+"?"+Math.floor((new Date).valueOf()/6048e5)+1;document.getElementsByTagName("head")[0].appendChild(s);void 0;
+script="https://raw.github.com/Artemisia-gt/Tribal-Wars-Scripts/master/Player%20stats/player-stats.js";s=document.createElement("script");s.type="text/javascript";s.src=script+"?"+Math.floor((new Date).valueOf()/6048e5)+2;document.getElementsByTagName("head")[0].appendChild(s);void 0;
