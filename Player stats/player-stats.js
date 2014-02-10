1192bb61bf6f695b5e1314ff24eb6531259f07ff
Artemisia-gt <prgiraud@gmail.com>
2014-02-11 00:40:43 +0100

    update stats

--- Player stats/player-stats.js
+++ Player stats/player-stats.js
@@ -1,5 +1,5 @@
-/** player-stats.js
- *  player's stats on profile (screen=info_players)
+/** stats.js
+ *  stats on player's or tribe's profile (screen=info_players OR screen=info_ally)
  *  author: Artemisia (artemisia@team.guerretribale.fr)
  *  game compatability: 8.18
  
@@ -23,11 +23,11 @@
 /* Lib */
 
 /**
- * returns player's id when visiting his profile
+ * returns player's or tribe id when visiting its profile
  * 
  * @return int $playerId
  */
-function getPlayerId() {
+function getId() {
     var playerId = $(location).attr('search').match( /[?&]id=([0-9]*)?/ );
     return parseInt( playerId[1] );
 }
@@ -39,19 +39,30 @@ function getPlayerId() {
  * @param enum(points, villages, oda, odd)  $type
  * @return string
  */
-function getStatsUrl(playerId, type) {
-    return "http://fr.twstats.com/" + game_data.world + "/image.php?type=playergraph&graph=" + type + "&id=" + playerId;
+function getStatsUrl(id, graph, type) {
+    return "http://fr.twstats.com/" + game_data.world + "/image.php?type=" + type + "graph&graph=" + graph + "&id=" + id;
 }
 
 /* Main */
 
-if(game_data.screen != 'info_player') {
-    UI.ErrorMessage('Ce script doit être lancé sur le profile d\'un joueur.', 5000);
+if(game_data.screen == 'info_player') {
+
+    var points      = getStatsUrl(getId(), 'points',    'player');
+    var villages    = getStatsUrl(getId(), 'villages',  'player');
+    var oda         = getStatsUrl(getId(), 'oda',       'player');
+    var odd         = getStatsUrl(getId(), 'odd',       'player');
+    
+    $('#content_value > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2)').prepend("<table><tr><td><table><tr><td>Evolution des Points</td></tr><tr><td><img width='250' height='200' src='" + points +"'> <br><center><input type='text' value='[img]" + points + "[/img]' onFocus='select();'></center></td></tr><tr><td>Evolutions des Villages</td></tr><tr><td><img width='250' height='200' src='" + villages +"'><center><input type='text' value='[img]" + villages + "[/img]' onFocus='select();'></center></td></tr></table></td><td><table><tr><td>Evolutions de l'ODA</td></tr><tr><td><img width='250' height='200' src='" + oda +"'> <center><input type='text' value='[img]" + oda + "[/img]' onFocus='select();'></center></td></tr><tr><td>Evolution ODD</td></tr><tr><td><img width='250' height='200' src='" + odd +"'><center><input type='text' value='[img]" + odd + "[/img]' onFocus='select();'></center></td></tr></table></td></tr></table>");
+
+} else if(game_data.screen == 'info_ally') {
+    
+    var points      = getStatsUrl(getId(), 'points',    'tribe');
+    var villages    = getStatsUrl(getId(), 'villages',  'tribe');
+    var oda         = getStatsUrl(getId(), 'oda',       'tribe');
+    var odd         = getStatsUrl(getId(), 'odd',       'tribe');
+    
+    $('#content_value > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2)').prepend("<table><tr><td><table><tr><td>Evolution des Points</td></tr><tr><td><img width='250' height='200' src='" + points +"'> <br><center><input type='text' value='[img]" + points + "[/img]' onFocus='select();'></center></td></tr><tr><td>Evolutions des Villages</td></tr><tr><td><img width='250' height='200' src='" + villages +"'><center><input type='text' value='[img]" + villages + "[/img]' onFocus='select();'></center></td></tr></table></td><td><table><tr><td>Evolutions de l'ODA</td></tr><tr><td><img width='250' height='200' src='" + oda +"'> <center><input type='text' value='[img]" + oda + "[/img]' onFocus='select();'></center></td></tr><tr><td>Evolution ODD</td></tr><tr><td><img width='250' height='200' src='" + odd +"'><center><input type='text' value='[img]" + odd + "[/img]' onFocus='select();'></center></td></tr></table></td></tr></table>");
+
+} else {
+    UI.ErrorMessage('Ce script doit être lancé depuis le profil d\'un joueur ou d\'une tribu.', 5000);
 }
-
-var points      = getStatsUrl(getPlayerId(), 'points'   );
-var villages    = getStatsUrl(getPlayerId(), 'villages' );
-var oda         = getStatsUrl(getPlayerId(), 'oda'      );
-var odd         = getStatsUrl(getPlayerId(), 'odd'      );
-
-$('#content_value > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2)').prepend("<table><tr><td><table><tr><td>Evolution des Points</td></tr><tr><td><img width='250' height='200' src='" + points +"'> <br><center><input type='text' value='[img]" + points + "[/img]' onFocus='select();'></center></td></tr><tr><td>Evolutions des Villages</td></tr><tr><td><img width='250' height='200' src='" + villages +"'><center><input type='text' value='[img]" + villages + "[/img]' onFocus='select();'></center></td></tr></table></td><td><table><tr><td>Evolutions de l'ODA</td></tr><tr><td><img width='250' height='200' src='" + oda +"'> <center><input type='text' value='[img]" + oda + "[/img]' onFocus='select();'></center></td></tr><tr><td>Evolution ODD</td></tr><tr><td><img width='250' height='200' src='" + odd +"'><center><input type='text' value='[img]" + odd + "[/img]' onFocus='select();'></center></td></tr></table></td></tr></table>");
